3be85d221b4a8f61ca8a5a309fa27b305406a839
Xbalanqué <xbalanque.velazquez@gmail.com>
2025-07-04 12:59:58 -0600

    Fix: use puppeteer-core instead of puppeteer

--- scrapeOktaStatusLogic.js
+++ scrapeOktaStatusLogic.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const cheerio = require('cheerio'); // Importa Cheerio
 
 async function scrapeOktaStatusLogic() {

1993f195c4c78bf868a97226adbdeb0362fc5a40
Xbalanqué <xbalanque.velazquez@gmail.com>
2025-07-04 21:38:40 -0600

    Ajuste colocar update json

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const express = require('axios');
 // Importa la lógica de scraping que ya tienes.
 // Asegúrate de que la ruta al archivo sea correcta.
 const { scrapeOktaStatusLogic } = require('./scrapeOktaStatusLogic'); // <-- CAMBIA ESTO SI TU ARCHIVO SE LLAMA DIFERENTE
@@ -13,6 +14,20 @@ app.get('/', async (req, res) => {
   try {
     const data = await scrapeOktaStatusLogic();
     console.log('Scraping successful.');
+
+    const bin_ID = '686879fe8a456b7966bb89fb';
+
+    //https://api.jsonbin.io/v3/b/686879fe8a456b7966bb89fb/latest
+    //requiere X-Master-Key or X-Access-Key in the header to read a private bin
+
+    await axios.put('https://api.jsonbin.io/v3/b/'+bin_ID, data, {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Master-Key': '$2a$10$3iRNcmntH4jH.OYcEARxhua.ErD0TuzsHy/ygVje1uNCvoyiPX/..'
+      }
+    });
+
+
     res.status(200).json(data);
   } catch (error) {
     console.error('Error during scraping process:', error);
